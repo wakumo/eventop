9567d71ca1f46b7ce5c98e1c54b6ddb2d64f9b4e
longhoang.wkm <longhoang@wakumo.vn>
2022-11-30 10:01:18 +0700

    fix: naming

--- src/scripts/seeds/events/events.ts
+++ src/scripts/seeds/events/events.ts
@@ -2,7 +2,7 @@ export const contractEvents = [
   {
     "service_name": "ctn",
     "name": "CampaignCreated(bytes32,bytes16,address,address,address,uint8)",
-    "abi": '{ "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "campaignId", "type": "bytes32" }, { "indexed": false, "internalType": "bytes16", "name": "salt", "type": "bytes16" }, { "indexed": false, "internalType": "address", "name": "creator", "type": "address" }, { "indexed": false, "internalType": "address", "name": "campaign", "type": "address" }, { "indexed": false, "internalType": "address", "name": "token", "type": "address" }, { "indexed": false, "internalType": "enum CampaignCTNStorage.TOKEN_TYPE", "name": "tokenType_", "type": "uint8" } ], "name": "CampaignCreated", "type": "event" }',
+    "abi": '{ "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "campaignId", "type": "bytes32" }, { "indexed": false, "internalType": "bytes16", "name": "salt", "type": "bytes16" }, { "indexed": false, "internalType": "address", "name": "creator", "type": "address" }, { "indexed": false, "internalType": "address", "name": "campaign", "type": "address" }, { "indexed": false, "internalType": "address", "name": "token", "type": "address" }, { "indexed": false, "internalType": "enum CampaignCTNStorage.TOKEN_TYPE", "name": "tokenType", "type": "uint8" } ], "name": "CampaignCreated", "type": "event" }',
     "chain_ids": [97, 8001]
   },
   {
