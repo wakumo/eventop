2094607d81eab9668afb09ee90c8d1a7236c5ae1
longhoang.wkm <longhoang@wakumo.vn>
2022-12-09 11:35:39 +0700

    feat: update events

--- src/scripts/seeds/events/events.ts
+++ src/scripts/seeds/events/events.ts
@@ -7,8 +7,8 @@ export const contractEvents = [
   },
   {
     "service_name": "ctn",
-    "name": "CampaignRan(bytes32,uint256,address)",
-    "abi": '{ "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "campaignId", "type": "bytes32" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }, { "indexed": false, "internalType": "address", "name": "sender", "type": "address" } ], "name": "CampaignRan", "type": "event" }',
+    "name": "CampaignRan(bytes32,uint256,uint256,uint256,address)",
+    "abi": '{ "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "campaignId", "type": "bytes32" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "adminFee", "type": "uint256" }, { "indexed": false, "internalType": "uint256", "name": "offerOwnerFee", "type": "uint256" }, { "indexed": false, "internalType": "address", "name": "sender", "type": "address" } ], "name": "CampaignRan", "type": "event" }',
     "chain_ids": [97, 8001]
   },
   {
@@ -21,6 +21,12 @@ export const contractEvents = [
     "service_name": "ctn",
     "name": "CampaignWithdrew(bytes32,address,address,uint256)",
     "abi": '{ "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "campaignId", "type": "bytes32" }, { "indexed": false, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "address", "name": "token", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "CampaignWithdrew", "type": "event" }',
+    "chain_ids": [97, 8001]
+  },
+  {
+    "service_name": "ctn",
+    "name": "FeeClaimed(bytes32,address,address,uint256)",
+    "abi": '{ "anonymous": false, "inputs": [ { "indexed": false, "internalType": "bytes32", "name": "campaignId", "type": "bytes32" }, { "indexed": false, "internalType": "address", "name": "tokenFee", "type": "address" }, { "indexed": false, "internalType": "address", "name": "claimer", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "amount", "type": "uint256" } ], "name": "FeeClaimed", "type": "event" }',
     "chain_ids": [97, 8001]
   }
 ]
