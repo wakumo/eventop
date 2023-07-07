2fa3e2250488e5ef8ea20cd6da3270f4efe1b2fd
longhoang.wkm <longhoang@wakumo.vn>
2023-07-07 17:39:16 +0700

    fix: syntax

--- src/apis/network/network.module.ts
+++ src/apis/network/network.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { NetworkService } from './network.service';
+import { NetworkService } from './network.service.js';
 
 @Module({
   providers: [NetworkService]
