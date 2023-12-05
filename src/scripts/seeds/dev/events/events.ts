e908c9f82d2c2b6d432c8f27c7137e4c1e35552b
quantbwkm <30611933+quantbwkm@users.noreply.github.com>
2023-12-05 16:44:47 +0700

    fix: erc20 transfer event name (#138)

--- src/scripts/seeds/dev/events/events.ts
+++ src/scripts/seeds/dev/events/events.ts
@@ -144,7 +144,7 @@ export const contractEvents = [
   {
     "service_name": "wallet-activity",
     "routing_key": "avacuscc.events.wallet-activity.erc20_transfer",
-    "name": "event Transfer(address,address,uint256)", // event Transfer(address indexed from, address indexed to, uint256 value);
+    "name": "Transfer(address,address,uint256)", // event Transfer(address indexed from, address indexed to, uint256 value);
     "abi": `{ "anonymous": false, "inputs": [ { "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event" }`,
     "chain_ids": [80001, 97],
   },
